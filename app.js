9b6fac20b5a78c67d4293701a41d6e1096bc3249
badjavii <128875626+Badjavii@users.noreply.github.com>
2025-02-07 00:21:56 -0400

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-let boxes = document.querySelectorAll(".box1")
-
-for (let box1 of boxes){
-    box1.addEventListener("click", function() {
-        let url = this.DataTransferItem.url;
-        window.open(url, "_blank");
-    });
-}
